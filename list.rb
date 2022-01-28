8d2322aefb354243d17bd7f246f5bf4a91ddc936
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-01-28 18:51:02 -0500

    Add testing cases to list

--- list.rb
+++ list.rb
@@ -12,6 +12,16 @@ class MyList
       }
     end
 end
-
+# Create our list
 list = MyList.new(1, 2, 3, 4)
-p list
+
+# Test #all?
+puts(list.all? { |e| e < 5 }) # true
+puts(list.all? { |e| e > 5 }) # false
+
+# Test #any?
+puts(list.any? { |e| e == 2 }) # true
+puts(list.any? { |e| e == 5 }) # false
+
+# Test #filter
+print(list.filter { |e| e&.even? })
