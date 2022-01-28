eee439183fc2fc27b61c2f5fa15861e80963dadd
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-01-28 17:59:16 -0500

    Create each method and MyEnumerable module

--- list.rb
+++ list.rb
@@ -1,9 +1,16 @@
 require_relative 'enumerable'
 
 class MyList
+  include MyEnumerable
   def initialize(*list)
     @list = *list
   end
+
+  def each
+      @list.each {
+
+      }
+    end
 end
 
 list = MyList.new(1, 2, 3, 4)
